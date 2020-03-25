064f3e20bd58c701718b0533100da14c96aeeb4f
yjcho <jinjanic91@gmail.com>
2020-03-25 23:46:37 +0900

    let and const

---
+++ 07.ES6/script.js
@@ -0,0 +1,72 @@
+ // Lecture: let and const
+
+/*
+ // ES5 
+var name5 = 'Jane Smith';
+var age5 = 23;
+name5 = 'Jane Miller';
+console.log(name5);
+
+// ES6 
+const name6 = 'Jane Smith';
+let age6 =23;
+name6 = 'Jane Miller';
+console.log(name6);
+*/
+
+// ES5 (function scoped)
+function driverLicense5(passedTest) {
+	if (passedTest) {
+		console.log(firstName); //undefined
+		var firstName = 'John';
+		var yearOfBirth = 1990;
+	}
+
+	console.log(firstName + ', born in '+yearOfBirth+', is now officially allowed to drive a car');
+}
+/* this is not work! because var is function scope!
+console.log(firstName + ', born in '+yearOfBirth+', is now officially allowed to drive a car');*/
+
+driverLicense5(true);
+
+// ES6 (block scoped)
+function driverLicense6(passedTest) {
+	
+	//console.log(firstName); --> error (cannot access)
+	let firstName = 'John';
+	const yearOfBirth = 1990; 
+
+	if (passedTest) {
+		firstName = 'John';
+	}
+
+	console.log(firstName + ', born in '+yearOfBirth+', is now officially allowed to drive a car'); //--> this will be error because let or const is block scope!
+}
+
+driverLicense6(true);
+
+//var i = 23; both i is var, then the result will be 5 (the last one.)
+let i = 23;
+
+for (let i=0; i < 5; i++) {
+	console.log(i); // 0, 1, 2, 3, 4
+}
+
+console.log(i); //23 (because of block scoped!) It's completely diff variable.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
