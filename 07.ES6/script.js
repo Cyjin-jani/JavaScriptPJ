f753b02341f23095afd447a6e67f64ba4e0b1830
yjcho <jinjanic91@gmail.com>
2020-03-31 00:02:35 +0900

    arrow func basic

--- 07.ES6/script.js
+++ 07.ES6/script.js
@@ -77,9 +77,8 @@ for (let i=0; i < 5; i++) {
 //console.log(c);
 
 /////////////////////////////////////////////////
-
 // Lecture: Strings
-
+/*
 let firstName = 'john';
 let lastName = 'Smith';
 const yearOfBirth = 1990;
@@ -102,6 +101,48 @@ console.log(n.endsWith('Sm'));
 console.log(n.includes('oh'));
 console.log(firstName.repeat(5));
 console.log(`${firstName} `.repeat(3));
+
+*/
+
+/////////////////////////////////////////////////
+// Lecture: Arrow Functions
+
+const years = [1990, 1965, 1982, 1937];
+
+// ES5
+
+var ages5 = years.map(function(el) {
+	return 2016 - el;
+});
+console.log(ages5);
+
+// ES6
+let ages6 = years.map(el => 2016 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`);
+
+ console.log(ages6);
+
+ages6 = years.map((el, index) => {
+	const now = new Date().getFullYear();
+	const age = now - el;
+	return `Age element ${index + 1}: ${age}.`
+});
+
+console.log(ages6);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
