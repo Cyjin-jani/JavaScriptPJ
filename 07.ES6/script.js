02455b9facc3cc650606b3263c072b9aeaf273af
yjcho <jinjanic91@gmail.com>
2020-04-15 23:51:22 +0900

    rest param

--- 07.ES6/script.js
+++ 07.ES6/script.js
@@ -336,7 +336,7 @@ console.log(ages.find(cur => cur >= 18)); //for value
 
 /////////////////////////////////////////////////////
 // Lecture : Spread operator
-
+/*
 function addFourAges (a, b, c, d) {
 	return a + b + c + d;
 }
@@ -366,17 +366,64 @@ const all = [h, ...boxes];
 
 Array.from(all).forEach(cur => cur.style.color = 'purple');
 
+*/
 
 
+/////////////////////////////////////////////////////
+// Lecture : Rest parameters
+
+/*
+// ES5
+function isFullAge5() {
+	//console.log(arguments);
+	var argsArr = Array.prototype.slice.call(arguments);
+
+	argsArr.forEach(function(cur) {
+		console.log((2016 - cur) >= 18);
+	});
+
+}
+
+//isFullAge5(1990, 1999, 1965);
+//isFullAge5(1990, 1999, 1965, 2016, 1987);
 
 
+// ES6
+
+function isFullAge6(...years) {
+	//console.log(years);
+	years.forEach(cur => console.log((2016 - cur) >= 18));
+}
+
+isFullAge6(1990, 1999, 1965, 2016, 1987);
+*/
 
 
+// ES5
+function isFullAge5(limit) {
+	//console.log(arguments);
+	var argsArr = Array.prototype.slice.call(arguments, 1);
+
+	//console.log(argsArr);
+
+	argsArr.forEach(function(cur) {
+		console.log((2016 - cur) >= limit);
+	});
+
+}
+
+//isFullAge5(21, 1990, 1999, 1965);
+//isFullAge5(1990, 1999, 1965, 2016, 1987);
 
 
+// ES6
 
+function isFullAge6(limit, ...years) {
+	//console.log(years);
+	years.forEach(cur => console.log((2016 - cur) >= limit));
+}
 
-
+isFullAge6(21, 1990, 1999, 1965, 2016, 1987);
 
 
 
