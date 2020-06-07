45195f46bfed6f1046a859c3ff9fa84c372d2fc9
yjcho <jinjanic91@gmail.com>
2020-06-07 17:36:48 +0900

    finished build search controller

--- 09.ModernJs/src/js/index.js
+++ 09.ModernJs/src/js/index.js
@@ -1,4 +1,33 @@
-// Global app controller
-import num from './test';
+import Search from './models/Search';
 
-console.log(`I imported ${num} from another module!`);
+/*Global state of the app 
+* - search object
+* - current recipe object
+* - shopping list object
+* - liked recipes
+*/
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = 'pizza'; //TODO 
+
+    if (query) {
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) render results on UI
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
