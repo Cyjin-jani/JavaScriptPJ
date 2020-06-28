33ea9195fb33823145ded8bb9c4d18f9a005e8c6
yjcho <jinjanic91@gmail.com>
2020-06-28 23:46:42 +0900

    finished setting localStorage

--- 09.ModernJs/src/js/index.js
+++ 09.ModernJs/src/js/index.js
@@ -154,10 +154,6 @@ elements.shopping.addEventListener('click', e => {
 LIKE CONTROLLER
 */
 
-//for Testing
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
-
 const controlLike = () => {
     if (!state.likes) state.likes = new Likes();
     const currentID = state.recipe.id;
@@ -194,7 +190,19 @@ const controlLike = () => {
 
 };
 
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+    
+    //Restorage likes
+    state.likes.readStorage();
+    
+    //Toggle like menu btn
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
 
+    // Render the existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 
 // Handling Recipe button clicks
